8f26a4b386f2568ab8a6aaf85f48f87f2d0b826b
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-17 18:16:08 +0200

    Create a dummy resolver, typedef and server

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    ping: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    ping: () => 'pong'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`Server running on ${url}`);
+});
