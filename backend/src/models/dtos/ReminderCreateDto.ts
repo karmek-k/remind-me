99de268680f5f81cef7a3d5e245f7250b94a2b57
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-17 22:34:56 +0200

    Add validators to ReminderCreateDto

--- backend/src/models/dtos/ReminderCreateDto.ts
+++ backend/src/models/dtos/ReminderCreateDto.ts
@@ -1,12 +1,15 @@
 import { InputType, Field } from 'type-graphql';
 import { Channel } from '../Reminder';
+import { MaxLength } from 'class-validator';
 
 @InputType()
 export class ReminderCreateDto {
   @Field()
+  @MaxLength(30)
   title!: string;
 
   @Field({ nullable: true })
+  @MaxLength(255)
   message!: string;
 
   @Field(() => [Channel])
