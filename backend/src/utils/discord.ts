79e30e8c5dfb76bd8d84284db4426c6249c5f3e7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-19 00:00:57 +0200

    Send discord embeds instead of plain text

--- backend/src/utils/discord.ts
+++ backend/src/utils/discord.ts
@@ -1,10 +1,19 @@
 import axios from 'axios';
 import { Reminder } from '../models/Reminder';
 
+export interface Embed {
+  title?: string;
+  description?: string;
+}
+
 export async function sendDiscordMessage(webhook: string, reminder: Reminder) {
   const { title, message } = reminder;
+  const embed: Embed = {
+    title,
+    description: message
+  };
 
   await axios.post<void>(webhook, {
-    content: `${title}:\n${message}`
+    embeds: [embed]
   });
 }
