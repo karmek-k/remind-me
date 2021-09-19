d00f6a14e60c743af1cdcd5d7107077770bd75ba
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-19 17:52:30 +0200

    Rename schedule to queue and delete the impl

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,13 +5,9 @@ import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import ReminderResolver from './resolvers/ReminderResolver';
-import { scheduleService } from './services/scheduleService';
 import { loggerService } from './services/loggerService';
 
 async function init() {
-  const jobCount = await scheduleService.countJobs();
-  loggerService.log(`Job count: ${jobCount}`);
-
   const schema = await buildSchema({ resolvers: [ReminderResolver] });
   const server = new ApolloServer({ schema });
 
