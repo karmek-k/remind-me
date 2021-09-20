6c14200959d292166c156a310bc77ee8cb1cbfda
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-20 12:10:36 +0200

    Add addJob and removeJob mutations

--- backend/src/resolvers/ReminderJobResolver.ts
+++ backend/src/resolvers/ReminderJobResolver.ts
@@ -1,4 +1,5 @@
-import { Arg, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Int, Mutation, Query, Resolver } from 'type-graphql';
+import { ReminderJobCreateDto } from '../models/dtos/ReminderJobCreateDto';
 import { ReminderJob } from '../models/ReminderJob';
 import { reminderJobProvider } from '../services/providers/reminderJob';
 
@@ -7,5 +8,15 @@ export default class {
   @Query(() => [ReminderJob])
   jobsForReminder(@Arg('reminderId', () => Int) reminderId: number) {
     return reminderJobProvider.findForReminder(reminderId);
+  }
+
+  @Mutation(() => ReminderJob)
+  async addJob(@Arg('reminderJobData') reminderJobData: ReminderJobCreateDto) {
+    return await reminderJobProvider.insert(reminderJobData);
+  }
+
+  @Mutation(() => ReminderJob, { nullable: true })
+  async removeJob(@Arg('jobId') jobId: number) {
+    return await reminderJobProvider.delete(jobId);
   }
 }
