f4d9df3b01d89f4e20aee8b4e7aac87fabf83467
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-25 14:30:15 +0200

    Fix jwtAuthContext args, implement jwtAuthChecker

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -3,19 +3,33 @@ import { AuthChecker } from 'type-graphql';
 import { User } from '../models/User';
 import { authService } from '../services/auth';
 
-export const jwtAuthContext = async (req: Request, res: Response) => {
-  const header = req.headers.authorization || '';
-  const token = header.split(' ')[1];
+interface ContextType {
+  user: User | null;
+}
+
+interface ContextReqRes {
+  req: Request;
+  res: Response;
+}
+
+export const jwtAuthContext = async ({ req }: ContextReqRes) => {
+  let token: string;
+  try {
+    const header = req.headers.authorization ?? '';
+    token = header.split(' ')[1];
+  } catch (err) {
+    return { user: null };
+  }
 
   const payload = authService.verifyJwt(token);
   if (!payload) {
     return { user: null };
   }
 
-  return { user: await User.findOne(payload.id) };
+  const user = (await User.findOne(payload.id)) ?? null;
+  return { user };
 };
 
-export const jwtAuthChecker: AuthChecker = async data => {
-  // TODO: implement
-  return true;
+export const jwtAuthChecker: AuthChecker<ContextType> = async ({ context }) => {
+  return context.user !== null;
 };
