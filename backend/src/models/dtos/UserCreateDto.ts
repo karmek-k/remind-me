37ada04e1cc70dff50154c470ccd9f9b24e08e18
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-28 19:17:01 +0200

    Add GraphQL docs for UserResolver and related DTOs

--- backend/src/models/dtos/UserCreateDto.ts
+++ backend/src/models/dtos/UserCreateDto.ts
@@ -1,5 +1,7 @@
 import { InputType } from 'type-graphql';
 import { UserLoginDto } from './UserLoginDto';
 
-@InputType()
+@InputType({
+  description: 'User credentials DTO used for creating a new user.'
+})
 export class UserCreateDto extends UserLoginDto {}
--- backend/src/models/dtos/UserLoginDto.ts
+++ backend/src/models/dtos/UserLoginDto.ts
@@ -2,13 +2,13 @@ import { Length } from 'class-validator';
 import { InputType, Field } from 'type-graphql';
 import { Dto } from './Dto';
 
-@InputType()
+@InputType({ description: 'User credentials DTO used for logging in.' })
 export class UserLoginDto implements Dto {
-  @Field()
+  @Field({ description: '3-20 characters.' })
   @Length(3, 20)
   username!: string;
 
-  @Field()
+  @Field({ description: '5-64 characters.' })
   @Length(5, 64)
   password!: string;
 }
