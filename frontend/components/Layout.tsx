e594ca8e4e4c5254cce5fdd07adbe9a99e681343
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-10-04 17:05:15 +0200

    Change the internals of Layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import React from 'react';
 
 interface Props {
-  title?: string;
+  title: string;
   header?: string;
   children?: any;
 }
@@ -11,13 +11,15 @@ const Layout: React.FC<Props> = ({ title, children, header }) => {
   return (
     <>
       <Head>
-        <title>{title && `${title} - `}Remind Me</title>
+        <title>{title} - Remind Me</title>
         <meta name="description" content="Reminding you by all means!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex justify-center items-center h-screen flex-col">
-        <h1 className="text-4xl">{header ?? 'Remind Me'}</h1>
-        {children}
+        <header>
+          <h1 className="text-4xl">{header ?? title}</h1>
+        </header>
+        <div className="my-2">{children}</div>
       </main>
     </>
   );
