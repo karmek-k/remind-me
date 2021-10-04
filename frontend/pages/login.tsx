eda29337246ed333b2c00b6042c3713d9bf624f5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-10-04 18:51:03 +0200

    Add state values and a submit handler

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from 'next';
+import React, { FormEvent, useState } from 'react';
 
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <Layout title="Log in">
-      <form method="POST" className="flex-col">
+      <form method="POST" onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
         <input
           className="mx-2 border-2 rounded border-green-600"
           id="username"
           name="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
           required
         />
 
@@ -20,6 +30,8 @@ const Home: NextPage = () => {
           id="password"
           type="password"
           name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
           required
         />
 
