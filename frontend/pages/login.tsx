95f83bdd66e3d7d2de1d7f64abdb78a6958f61e7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-10-04 17:24:09 +0200

    Build the login form

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -3,7 +3,34 @@ import type { NextPage } from 'next';
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
-  return <Layout title="Log in"></Layout>;
+  return (
+    <Layout title="Log in">
+      <form method="POST" className="flex-col">
+        <label htmlFor="username">Username</label>
+        <input
+          className="mx-2 border-2 rounded border-green-600"
+          id="username"
+          name="username"
+          required
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          className="mx-2 border-2 rounded border-green-600"
+          id="password"
+          type="password"
+          name="password"
+          required
+        />
+
+        <input
+          className="py-2 px-4 rounded bg-green-600 text-white cursor-pointer"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </Layout>
+  );
 };
 
 export default Home;
