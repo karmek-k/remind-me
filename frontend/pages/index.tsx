3832901af0280b7cb1ed2aeca511ffba56a70e04
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-10-04 17:05:29 +0200

    Create login page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
-  return <Layout title="Home page"></Layout>;
+  return (
+    <Layout title="Home page">
+      <ul>
+        <li>
+          <Link href="/login">Log in</Link>
+        </li>
+      </ul>
+    </Layout>
+  );
 };
 
 export default Home;
